f7eb636f00e8dca6693828a639ab0c02c8a9ba8c
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-06 04:57:26 -0800

    function to check for evennes or oddness

--- iseven.js
+++ iseven.js
@@ -5,11 +5,4 @@ function checkEven(num) {
   //base case
   if (num === 0) return true
   if (num === 1) return false
-
-  //recursive case
-  if (num < 0) return true
-  return checkEven(num -2)
- }
-console.log(checkEven(0))
-console.log(checkEven(1))
-console.log(checkEven(-3))
+}
