aaff65bf201880daceac60e7e8c9141e6e6a551d
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-06 04:59:40 -0800

    function to check for evennes or oddness recursively

--- iseven.js
+++ iseven.js
@@ -1,6 +1,3 @@
-// 1)write a function called isEven which checks if a number is even or odd
-// Hint: 0 is even and 1 is odd, for any other nummber N, its eveness is the same as N-2.
-// Notice how this works when N is negative and try to fix it.
 function checkEven(num) {
   //base case
   if (num === 0) return true
