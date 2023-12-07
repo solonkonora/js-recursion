9fbb65847909ae0688b665729ae885ed12437123
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-06 18:24:31 -0800

    linter errors

--- iseven.js
+++ iseven.js
@@ -8,26 +8,26 @@ Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a
 way to fix this?*/
 
 function checkEven(num) {
+  // base case
+  if (num === 0) return true;
+  if (num === 1) return false;
 
-  //base case
-  if (num === 0) return true
-  if (num === 1) return false
+  // recursive case
+  if (num < 0) return true;
+  return checkEven(num - 2);
+}
 
-  //recursive case
-  if (num < 0) return true
-  return checkEven(num -2)
- }
-console.log(checkEven(50))
-console.log(checkEven(75))
-console.log(checkEven(-1))
+console.log(checkEven(50));
+console.log(checkEven(75));
+console.log(checkEven(-1));
 
-//2) power function
-function power (base, exponent) {
+// 2) power function
+function power(base, exponent) {
   if (exponent === 0) {
-    return 1
-  }
-  else {
-    return base * power(base, exponent - 1)
+    return 1;
+  } else {
+    return base * power(base, exponent - 1);
   }
 }
-console.log(power(2, 4))
+
+console.log(power(2, 4));
