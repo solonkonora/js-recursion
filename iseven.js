39decdb2d7a32b7cb5fb2cef1d6a302ca2fdfe88
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-12-06 17:48:20 -0800

    add some content

--- iseven.js
+++ iseven.js
@@ -1,3 +1,12 @@
+/*• Zero is even.
+• One is odd.
+• For any other number N, its evenness is the same as N - 2.
+Define a recursive function isEven corresponding to this description. The
+function should accept a single parameter (a positive, whole number) and return
+a Boolean.
+Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a
+way to fix this?*/
+
 function checkEven(num) {
 
   //base case
@@ -8,9 +17,9 @@ function checkEven(num) {
   if (num < 0) return true
   return checkEven(num -2)
  }
-console.log(checkEven(0))
-console.log(checkEven(1))
-console.log(checkEven(-3))
+console.log(checkEven(50))
+console.log(checkEven(75))
+console.log(checkEven(-1))
 
 //2) power function
 function power (base, exponent) {
